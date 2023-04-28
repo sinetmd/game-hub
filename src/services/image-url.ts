023d0d45a9eb736d219a2c820c059fc215208932
sinetmd <dobremrn12@gmail.com>
2023-04-28 11:13:01 +0300

    Get optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,8 @@
+// we are going to cropp the image url for better performance
+const getCroppedImageUrl = (url: string) => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length; // add the media too
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+
+export default getCroppedImageUrl;
