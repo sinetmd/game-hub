cde27fa22a5c6408df870c13da305b356049c368
sinetmd <dobremrn12@gmail.com>
2023-05-04 19:09:59 +0300

    minor fixes

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "ac22c67521b24767a9b8127945b0ce1f",
+    key: "", // enter your rawg key here
   },
 });
 
