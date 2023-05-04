957ea693920c25bf4d42376521a11ff30ada05ec
sinetmd <dobremrn12@gmail.com>
2023-05-04 17:19:37 +0300

    Game build attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,11 +1,14 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
-
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
+  genres: Genre[];
+  publishers: Publisher[];
   background_image: string;
-  parent_platforms: { platform: Platform; }[];
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
   rating: number;
